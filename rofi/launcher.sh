de248defaf3a1bab6055dda2e3d7457e5d9b4b51
Nicolas JOULIN <joulin.nicolas@gmail.com>
2024-11-16 17:04:04 +0100

    Hyprland dots

--- rofi/launcher.sh
+++ rofi/launcher.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-theme="$HOME/.config/rofi/launcher.rasi"
+theme="$dir/launcher.rasi"
 
 rofi -no-lazy-grab -show run \
 -modi run,drun,window \
--theme $dir/"$theme"
+-theme $theme
